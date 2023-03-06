f90e4718013e2347a222a2e15aebc50cb69a162f
fredowo20 <103700122+fredowo20@users.noreply.github.com>
2023-03-06 18:50:53 -0300

    Add files via upload

--- KP_Methods/Python/Element_Based_2.py
+++ KP_Methods/Python/Element_Based_2.py
@@ -68,3 +68,35 @@ items = [[1, 1, 0], [1, 0, 1], [0, 1, 1]]
 profits = [60, 100, 120]
 max_weight = 50
 print(knapsack_by_elements2(elements, items, profits, max_weight))
+
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+import numpy as np
+
+# Selection of instance
+data = pd.read_csv(r'C:\Users\Javier\Documents\IA\Fondecyt\KP_Methods\Instances\InstancesofSetI\85_100_0.10_0.75.txt', header=None)
+
+n_items = data[0][0]
+n_elements = data[0][1]
+max_weight = int(data[0][2])
+
+profits = str(data[0][3]).split()
+for i in range(len(profits)):
+    profits[i] = int(profits[i])
+
+elements = str(data[0][4]).split()
+for i in range(len(elements)):
+    elements[i] = int(elements[i])
+
+items = data.tail(-5)
+items.reset_index(inplace = True, drop = True)
+
+for i in range(len(items)):
+    items[0][i] = str(items[0][i]).split()
+for i in range(len(items)):
+    for j in range(len(elements)):
+        items[0][i][j] = int(items[0][i][j])
+
+items = items[0].values.tolist()
+
+print(knapsack_by_elements2(elements, items, profits, max_weight))
